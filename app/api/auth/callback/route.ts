a94450991341ceedc43111abd97b957cfed4ca7f
Deepacks <vladimirsette.vc@gmail.com>
2023-12-24 00:13:44 +0100

    add redis and test action vars

---
+++ app/api/auth/callback/route.ts
@@ -0,0 +1,31 @@
+import { redirect } from 'next/navigation'
+import { NextRequest, NextResponse } from 'next/server'
+// import { redisClient } from '@/clients'
+
+export async function GET(request: NextRequest) {
+  // const finalState = request.nextUrl.searchParams.get('state')!
+  // const initialState = await redisClient.get(finalState)
+
+  // if (!initialState) {
+  //   redirect('/')
+  // }
+
+  // await redisClient.del(finalState)
+
+  // const error = request.nextUrl.searchParams.get('error')
+  // if (error) {
+  //   redirect('/')
+  // }
+
+  // const code = request.nextUrl.searchParams.get('code')
+
+  // const requestDto = {
+  //   code: code,
+  //   redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+  //   grant_type: 'authorization_code',
+  // }
+
+  const testo = process.env.TEST ?? 'miao'
+
+  return NextResponse.json(testo)
+}
--- app/api/auth/spotify/route.ts
+++ app/api/auth/spotify/route.ts
@@ -2,22 +2,19 @@ import { RedirectType, redirect } from 'next/navigation'
 import querystring from 'querystring'
 import { generateState } from '@/utils'
 
-export function GET() {
-  console.log('call')
-
-  const clientID = process.env.SPOTIFY_CLIENT_ID
-  const redirectUri = process.env.SPOTIFY_REDIRECT_URI
-
+export async function GET() {
   const scope = 'user-read-private user-read-email'
   const state = generateState(16)
+
+  // await redisClient.set(state, 'valid')
 
   const spotifyAuthUri =
     'https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
 
-      client_id: clientID,
-      redirect_uri: redirectUri,
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
 
       scope,
       state,
